4494c243c73dfe3b58b38ef0095cca96f9fe9b1a
kakapo666 <haiming.cheng@aelf.io>
2021-06-04 11:49:42 +0800

    add

--- tests/SLToken-c/Deploy-test.js
+++ tests/SLToken-c/Deploy-test.js
@@ -1,18 +1,20 @@
-const comptroller = artifacts.require("Comptroller");
-
+const fs = require('fs');
+const Comptroller = artifacts.require("Comptroller");
+const Unitroller = artifacts.require("Unitroller");
 
 
 contract('SLToken deploy',([kakapo,bob])=>{
     beforeEach(async ()=>{
-
+        const data = fs.readFileSync('../../deploy/config/Comptroller.json');
+        const config =JSON.parse(data).config;
+        this.comptroller =await Comptroller.new({from:kakapo});
+        // this.unitroller =await Unitroller.new({from:kakapo});
+        // await console.log(this.unitroller.address);
     })
 
-
-    it('should deploy SLErc20 success', function () {
-            console.log(123);
+    it('should deploy SLErc20 success', async ()=>{
     });
 
-    it('should deploy SLToken success', function () {
-
+    it('should deploy SLToken success', async ()=>{
     });
 });
--- truffle-config.js
+++ truffle-config.js
@@ -19,8 +19,8 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-      // gas: 5002388,
-      // gasPrice: 30000000000
+      gas: 3012388,
+      gasPrice: 30000000000
     }
     // kovan: {
     //   provider: new HDWalletProvider(keys, "https://kovan.infura.io/v3/" + infura_apikey, 0, 4),
