96b9c9754c4ac29e99db779cf9c489b9999a4aa4
kakapo666 <haiming.cheng@aelf.io>
2021-06-11 14:13:40 +0800

    add SLEther support market  test

--- tests/SLToken-Migrator/3_SLToken._supportMarks.js
+++ tests/SLToken-Migrator/3_SLToken._supportMarks.js
@@ -4,10 +4,14 @@ const data = fs.readFileSync("../../deploy/Address.json");
 const addresses = JSON.parse(data);
 
 contract("Comptroller ",([kakapo])=>{
-    it('should support SLToken', async ()=> {
+    it('should support SLToken SLErc20 & SLEther', async ()=> {
        this.comptroller =  await Comptroller.at(addresses["Unitroller"]);
        await this.comptroller._supportMarket(addresses["SLErc20Delegator_slUSDT"],{from:kakapo}).then(function (s) {
-           console.log("s:"+s);
+           console.log("SLErc20:"+s);
        });
+
+       await this.comptroller._supportMarket(addresses['SLEther_slHT'],{from:kakapo}).then(function (s) {
+           console.log("SLEther:"+s);
+       })
     });
 });
